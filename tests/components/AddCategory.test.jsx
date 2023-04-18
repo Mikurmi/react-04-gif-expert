5eb78e99542bbd467d3a9ce1785aa14a2549e81f
Adan <adangames19@gmail.com>
2023-04-18 08:30:44 +0200

    Primeras pruebas

---
+++ tests/components/AddCategory.test.jsx
@@ -0,0 +1,38 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { AddCategory } from "../../src/components/AddCategory";
+
+describe('Prueba del AddCategory', () => {
+
+    test('debe de cambiar el valor de la caja de texto', () => { 
+
+        render( <AddCategory onNewCategory={ () => {}} />);
+        const input = screen.getByRole('textbox');
+
+        fireEvent.input( input, { target: { value: 'Saitama' } } );
+        // screen.debug();
+    })
+
+    test('debe de llamar onNewCategory si el input tiene un valor', () => {
+
+        const inputValue = 'Saitama';
+        //Simulacion de la funcion
+        const onNewCategory = jest.fn();
+        //TODO: ???
+
+        render( <AddCategory onNewCategory={ onNewCategory} />);
+
+        const input = screen.getByRole('textbox');
+        const form = screen.getByRole('form');
+
+        fireEvent.input( input, { target: { value: inputValue } } );
+        fireEvent.submit( form );
+
+        expect( input.value ).toBe('');
+        // screen.debug();
+
+        //Evalua si ha sido llamada
+        expect( onNewCategory ).toHaveBeenCalled();
+        expect( onNewCategory ).toHaveBeenCalledTimes(1);
+        expect( onNewCategory ).toHaveBeenCalledWith( inputValue );
+    })
+})
